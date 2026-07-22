85cab07f8c6e21636872c85ab5e8b23d6c5117d1
Ramit Sarker <ramitsarker07@gmail.com>
2026-07-22 23:17:46 +0530

    Time: 2 ms (59.66%), Space: 19 MB (99.92%) - LeetHub

---
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def limitOccurrences(self, nums: list[int], k: int) -> list[int]:
+        res=[]
+        for i in range(len(nums)):
+            if nums[i] not in res:
+                res.append(nums[i])
+            else:
+                if res.count(nums[i])==k:
+                    i+=1
+                else:
+                    res.append(nums[i])
+        return res
+        
