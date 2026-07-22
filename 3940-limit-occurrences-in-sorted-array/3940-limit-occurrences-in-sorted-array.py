31456fc9e78328eef06aa20a23f3bff6367315ac
Ramit Sarker <ramitsarker07@gmail.com>
2026-07-22 23:21:04 +0530

    Time: 6 ms (14.89%), Space: 19.3 MB (33.28%) - LeetHub

--- 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.py
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.py
@@ -5,7 +5,9 @@ class Solution:
             if nums[i] not in res:
                 res.append(nums[i])
             else:
-                if res.count(nums[i])<k:
+                if res.count(nums[i])==k:
+                    continue
+                else:
                     res.append(nums[i])
         return res
         
