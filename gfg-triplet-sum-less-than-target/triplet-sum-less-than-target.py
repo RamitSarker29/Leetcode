3344eba7ba2d15c358f26b1328c82fa4b147571f
Ramit Sarker <ramitsarker07@gmail.com>
2026-07-23 21:12:31 +0530

    Update triplet-sum-less-than-target.py

--- gfg-triplet-sum-less-than-target/triplet-sum-less-than-target.py
+++ gfg-triplet-sum-less-than-target/triplet-sum-less-than-target.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def countTriplets(self, sum, arr):
+        #code here
+        count=0
+        arr.sort()
+        for i in range(len(arr)-2):
+            j=i+1
+            k=len(arr)-1
+            while (j<k):
+                current_sum=arr[i]+arr[j]+arr[k]
+                if current_sum<sum:
+                    count+=k-j
+                    j+=1
+                else:
+                    k-=1
+        return count
