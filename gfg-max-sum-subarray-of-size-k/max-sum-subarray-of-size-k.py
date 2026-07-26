e7a9041b3a82fd93a92e906c9a1cdea718a72bf5
Ramit Sarker <ramitsarker07@gmail.com>
2026-07-26 21:55:16 +0530

    Add maxSubarraySum method to Solution class
    Implemented a solution to find the maximum sum of a subarray of size k using a sliding window approach.

--- gfg-max-sum-subarray-of-size-k/max-sum-subarray-of-size-k.py
+++ gfg-max-sum-subarray-of-size-k/max-sum-subarray-of-size-k.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def maxSubarraySum(self, arr, k):
+        # code here 
+        i=0
+        j=k-1
+        sum1=0
+        max_sum=0
+        window_sum=sum(arr[0:k])
+        max_sum=window_sum
+        while (j<len(arr)-1):
+            window_sum=window_sum-arr[i]+arr[j+1]
+            max_sum=max(window_sum,max_sum)
+            i+=1
+            j+=1
+        return max_sum
+            
