edd395421e1b38aa35289adcd7769b1ab69b93a4
Ramit Sarker <ramitsarker07@gmail.com>
2026-07-27 20:28:50 +0530

    Time: 0 ms (100%), Space: 19.1 MB (92.4%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -3,11 +3,11 @@ class Solution:
         max1 = float('-inf')
         max2 = float('-inf')
 
-        for num in nums:
-            if num > max1:
+        for i in nums:
+            if i > max1:
                 max2 = max1
-                max1 = num
-            elif num > max2:
-                max2 = num
+                max1 = i
+            elif i > max2:
+                max2 = i
 
         return (max1 - 1) * (max2 - 1)
