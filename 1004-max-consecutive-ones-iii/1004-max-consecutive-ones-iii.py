67196626ddae192f506d31e8e4fc611aee5eef6e
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-03 01:45:17 +0530

    Time: 63 ms (38.99%), Space: 22.3 MB (22.7%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -2,15 +2,17 @@ class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         i=0
         j=0
-        hash_map={}
+        zero_count=0
         max_len=float('-inf')
         for j in range(len(nums)):
-            if nums[j] in hash_map:
-                hash_map[nums[j]]+=1
-            else:
-                hash_map[nums[j]]=1
-            while hash_map.get(0,0)>k:
-                hash_map[nums[i]]-=1
+            if nums[j]==0:
+                zero_count+=1
+            while zero_count>k:
+                if nums[i]==0:
+                    zero_count-=1
                 i+=1
             max_len=max(max_len,j-i+1)
         return max_len
+            
+
+        
