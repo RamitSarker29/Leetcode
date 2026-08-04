79a9535ff225d3e33afa6232f3ae9ded59d464b7
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-04 20:38:26 +0530

    Time: 2 ms (75.11%), Space: 19.2 MB (55.74%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -1,14 +1,11 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        hash_map={}
+        seen=set(nums)
         res=[]
-        for i in range(len(nums)):
-            if nums[i] not in hash_map:
-                hash_map[nums[i]]=1
         min_num=min(nums)
         max_num=max(nums)
         while min_num!=max_num:
-            if min_num +1 not in hash_map:
+            if min_num +1 not in seen:
                 res.append(min_num + 1)
             min_num+=1
         return res
