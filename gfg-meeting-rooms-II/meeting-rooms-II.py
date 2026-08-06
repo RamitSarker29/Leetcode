5628c7d98471721503c2858bbcdddfc5e9463249
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-07 00:44:46 +0530

    Update meeting-rooms-II.py

--- gfg-meeting-rooms-II/meeting-rooms-II.py
+++ gfg-meeting-rooms-II/meeting-rooms-II.py
@@ -1,1 +1,23 @@
+class Solution:
+    def minMeetingRooms(self, start, end):
+        # code here
+        
+        room = 0
+        min_room = 0
+        i=0
+        j=0
+        start.sort()
+        end.sort()
+        while i<len(start) and j<len(end) :
+            if start[i] < end[j]:
+                room+=1
+                i+=1
+            elif start[i] > end[j]:
+                room-=1
+                j+=1
+            else:
+                room -=1
+                j+=1
+            min_room = max(room,min_room)
+        return min_room
 
