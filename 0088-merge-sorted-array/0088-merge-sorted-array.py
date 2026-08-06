04e0b3d1f6165f063ed62dc964bd0d8f8ea7ced0
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-06 21:38:53 +0530

    Time: 0 ms (100%), Space: 19.2 MB (77.02%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,10 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i=m-1
-        j=n-1
-        k=m+n-1
-        while (j>=0):
+        i= m-1
+        j = n-1
+        k = m+n-1
+        while j>=0:
             if i>=0 and nums1[i]>nums2[j]:
                 nums1[k]=nums1[i]
                 i-=1
@@ -14,3 +14,4 @@ class Solution:
                 nums1[k]=nums2[j]
                 j-=1
             k-=1
+        
