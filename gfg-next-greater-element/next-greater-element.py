099b1e50bfc7173098ca995725b0339f5cb1e668
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-14 01:08:49 +0530

    Update next-greater-element.py

--- gfg-next-greater-element/next-greater-element.py
+++ gfg-next-greater-element/next-greater-element.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def nextLargerElement(self, arr):
+        # code here
+        stack = []
+        res =[-1]*len(arr)
+        for i in range(len(arr)-1,-1,-1):
+            while len(stack) !=0 and stack[-1] <= arr[i]:
+                stack.pop()
+            if len(stack) !=0 and stack[-1] > arr[i]:
+                res[i] = stack[-1]
+            stack.append(arr[i])
+        return res
