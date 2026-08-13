2d8d7e58c2e468fe8772f18fd6dc3934db4854be
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-14 01:35:53 +0530

    Update remove-two-consecutive-same.py

--- gfg-remove-two-consecutive-same/remove-two-consecutive-same.py
+++ gfg-remove-two-consecutive-same/remove-two-consecutive-same.py
@@ -1,1 +1,15 @@
+class Solution:
+    def removeConsecutiveSame(self, arr):
+        # code here
+        stack = []
+        for i in range(len(arr)):
+            if len(stack)!=0:
+                if stack[-1] == arr[i]: 
+                    stack.pop()
+                else:
+                    stack.append(arr[i])
+            else:
+                stack.append(arr[i])
+
+        return len(stack)
 
