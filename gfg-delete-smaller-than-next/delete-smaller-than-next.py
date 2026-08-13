fe531452ffdd8e252cee33463fc541ea4df6e48a
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-14 02:11:50 +0530

    Update delete-smaller-than-next.py

--- gfg-delete-smaller-than-next/delete-smaller-than-next.py
+++ gfg-delete-smaller-than-next/delete-smaller-than-next.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def deleteElement(self,arr,k):
+        # Code here
+        stack =[]
+        for i in range(len(arr)):
+            while len(stack) !=0 and stack[-1] < arr[i] and k!=0:
+                stack.pop()
+                k-=1
+            stack.append(arr[i])
+        return stack
+            
