742dfd1407220f7ec9efb2427485691ff1c51670
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-14 20:34:22 +0530

    Time: 0 ms (100%), Space: 19.1 MB (88.83%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
@@ -1,16 +1,15 @@
 class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
-        hash_map={}
-        i,j=0,0
-        max_len=0
+        max_len = 0
+        i,j = 0, 0
+        hash_map = {}
         for j in range(len(s)):
             if s[j] in hash_map:
-                hash_map[s[j]]+=1
+                hash_map[s[j]] +=1
             else:
-                hash_map[s[j]]=1
-            while hash_map[s[j]]>2:
-                hash_map[s[i]]-=1
+                hash_map[s[j]] = 1
+            while hash_map[s[j]] > 2:
+                hash_map[s[i]] -=1
                 i+=1
-            max_len=max(max_len,j-i+1)
+            max_len = max(max_len, j-i+1)
         return max_len
-        
