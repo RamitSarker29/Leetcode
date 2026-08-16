618b0cb222e56c325d4d18f75794dbddca127c08
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-17 02:01:50 +0530

    Update ceil-in-a-sorted-array.py

--- gfg-ceil-in-a-sorted-array/ceil-in-a-sorted-array.py
+++ gfg-ceil-in-a-sorted-array/ceil-in-a-sorted-array.py
@@ -1,1 +1,15 @@
+class Solution:
+    def findCeil(self, arr, x):
+        # code here
+        low = 0
+        high = len(arr) - 1
+        ans = -1
+        while high >= low :
+            mid = (low + high) // 2
+            if arr[mid] >= x:
+                ans = mid
+                high = mid - 1
+            if arr[mid] < x:
+                low = mid + 1
+        return ans
 
