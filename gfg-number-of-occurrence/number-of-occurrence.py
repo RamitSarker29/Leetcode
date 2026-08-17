b63234e480679e594fda69952cb7555c5c9b8978
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-18 02:06:31 +0530

    Implement countFreq method to count occurrences

--- gfg-number-of-occurrence/number-of-occurrence.py
+++ gfg-number-of-occurrence/number-of-occurrence.py
@@ -1,1 +1,28 @@
-
+class Solution:
+    def countFreq(self, arr, target):
+        # code here
+        first = -1
+        last = -1
+        low = 0
+        high = len(arr) - 1
+        while low <= high:
+            mid = (low + high) //2
+            if arr[mid] > target:
+                high = mid - 1
+            if arr[mid] < target:
+                low = mid + 1
+            if arr[mid] == target:
+                first = mid
+                high = mid - 1
+        low = 0
+        high = len(arr) - 1
+        while low <= high:
+            mid = (low + high) //2
+            if arr[mid] > target:
+                high = mid - 1
+            if arr[mid] < target:
+                low = mid + 1
+            if arr[mid] == target:
+                last = mid
+                low = mid + 1
+        return 0 if first == -1 and last == -1 else last - first + 1
