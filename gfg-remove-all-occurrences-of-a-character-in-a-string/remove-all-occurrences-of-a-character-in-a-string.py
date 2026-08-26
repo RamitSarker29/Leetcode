224b3e028d1c3e28536d52cc5ad27ea48d7f9b59
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-26 22:26:43 +0530

    Update remove-all-occurrences-of-a-character-in-a-string.py

--- gfg-remove-all-occurrences-of-a-character-in-a-string/remove-all-occurrences-of-a-character-in-a-string.py
+++ gfg-remove-all-occurrences-of-a-character-in-a-string/remove-all-occurrences-of-a-character-in-a-string.py
@@ -1,1 +1,23 @@
+class Solution:
+    # Function to remove all occurrences of the character from the string
+    def removeCharacter(self, s, c):
+        # code here
+        i = 0
+        j = 0
+        s = list(s)
+        while j < len(s) :
+            if s[j] != c :
+                s[i] = s[j]
+                i += 1
+            j += 1
+        return ''.join(s[:i])
 
+            
+            
+            
+            
+            
+            
+ 
+            
+        
