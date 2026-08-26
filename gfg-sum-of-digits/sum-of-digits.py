52124c96dfa0fd1ea2a584a181874f7ba3e1a1b6
Ramit Sarker <ramitsarker07@gmail.com>
2026-08-26 21:46:11 +0530

    Add sumOfDigits method to Solution class
    Implement recursive function to calculate sum of digits.

--- gfg-sum-of-digits/sum-of-digits.py
+++ gfg-sum-of-digits/sum-of-digits.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def sumOfDigits(self, n):
+        # code here
+        def fun(n):
+            if n == 0:
+                return 0
+            d = n % 10
+            n = n // 10
+            ans = fun(n)
+            return d + ans
+        return fun(n)
+        
