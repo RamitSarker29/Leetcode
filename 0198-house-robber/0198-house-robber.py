793c781eecfc43172ae1218bd80a3ec22040c731
Ramit Sarker <ramitsarker07@gmail.com>
2026-09-11 22:04:34 +0530

    Time: 0 ms (100%), Space: 19.3 MB (62.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,14 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = {}
-        def fun (n) :
-            if n == 0 :
-                return nums[0]
-            if n == 1 :
-                return max (nums[0] , nums[1])
-            if n in dp :
-                return dp[n]
-            dp[n] = max (fun(n - 2) + nums[n] , fun(n - 1))
-            return dp[n]
-        return fun (len(nums) - 1)
-        
+        prev2 = 0
+        prev1 = 0
+        for i in nums:
+            curr = max(prev1, i + prev2)
+            prev2 = prev1
+            prev1 = curr
+        return prev1
