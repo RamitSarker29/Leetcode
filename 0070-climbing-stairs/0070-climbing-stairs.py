129118fb22e4d9fef5ea128d7f72ef7a124d4c93
Ramit Sarker <ramitsarker07@gmail.com>
2026-09-11 21:03:18 +0530

    Time: 0 ms (100%), Space: 19.5 MB (17.77%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = {}
+        def fun (n) :
+            if n == 1 :
+                return 1
+            if n == 2 :
+                return 2
+            if n in dp :
+                return dp[n]
+            dp[n] = fun(n - 1) + fun(n - 2)
+            return dp[n]
+        return fun (n)
