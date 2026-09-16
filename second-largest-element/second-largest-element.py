3373fe2d6fc6c093e09e18333e09058017aa21af
Ramit Sarker <ramitsarker07@gmail.com>
2026-09-16 17:54:58 +0530

    Update second-largest-element.py

--- second-largest-element/second-largest-element.py
+++ second-largest-element/second-largest-element.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def secondLargestElement(self, nums):
+        largest1 = nums[0]
+        largest2 = -1
+        for i in range(len(nums)) :
+            if nums[i] > largest1 :
+                largest2 = largest1
+                largest1 = nums[i]
+            if nums[i] > largest2 and nums[i] < largest1 :
+                largest2 = nums[i]
+        return largest2
+        
