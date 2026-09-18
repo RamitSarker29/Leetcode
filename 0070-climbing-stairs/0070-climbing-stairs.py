fe9aa6c64201c27bc7ad9d48f6fdcf1cb212591c
Ramit Sarker <ramitsarker07@gmail.com>
2026-09-18 17:49:21 +0530

    Time: 0 ms (100%), Space: 19.3 MB (54.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,11 +3,12 @@ class Solution:
         dp = {}
         def fun (n) :
             if n == 1 :
-                return 1
+                return 1 
             if n == 2 :
                 return 2
             if n in dp :
-                return dp[n]
-            dp[n] = fun (n - 1) + fun (n - 2)
+                return dp [n]
+            dp [n] = fun (n - 1) + fun (n - 2)
             return dp[n]
         return fun (n)
+        
