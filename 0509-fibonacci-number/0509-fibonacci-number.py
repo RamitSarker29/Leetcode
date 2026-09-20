b4c840966c792026b0733f95f2d0ce7d5b88115c
Ramit Sarker <ramitsarker07@gmail.com>
2026-09-20 16:41:31 +0530

    Time: 368 ms (5%), Space: 19.3 MB (19.65%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,9 +4,8 @@ class Solution:
             if n == 0 :
                 return 0
             if n == 1 :
-                return 1 
-            ans1 = fun(n - 1)
-            ans2 = fun(n - 2)
-            return ans1 + ans2
-        return fun (n)
+                return 1
+            ans = fun(n-1) + fun(n - 2)
+            return ans
+        return fun(n)
         
